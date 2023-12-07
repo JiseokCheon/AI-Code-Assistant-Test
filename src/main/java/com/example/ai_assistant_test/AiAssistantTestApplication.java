86028797b35d1e72edcc3a7e556ec44caa573853
jiseok <cjswltjr159@naver.com>
2023-12-07 13:17:58 +0900

    init

---
+++ src/main/java/com/example/ai_assistant_test/AiAssistantTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_assistant_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAssistantTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAssistantTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ai_assistant_test/AiAssistantTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_assistant_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAssistantTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
