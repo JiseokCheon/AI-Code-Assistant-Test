64c46c5512a1acc57cf60236bc7418a0b3d86532
jiseok <cjswltjr159@naver.com>
2023-12-13 10:44:06 +0900

    feat: tabnine

--- java_demo/src/test/java/com/example/java_demo/PayrollTest.java
+++ java_demo/src/test/java/com/example/java_demo/PayrollTest.java
@@ -1,6 +1,15 @@
 package com.example.java_demo;
 
+import org.junit.jupiter.api.Test;
+
 public class PayrollTest {
+
+	// validatePhoneNumber Method에 대한 단위테스트코드를 작성해줘
+	@Test
+	public void validatePhoneNumber() {
+
+	}
+
 
 
 }
